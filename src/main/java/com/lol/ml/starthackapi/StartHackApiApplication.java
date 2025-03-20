0a0e7f58252a9388745447df4cc655c895771f6a
CodeC <58306894+CodeCLS@users.noreply.github.com>
2025-03-20 23:52:41 +0100

    test1

--- src/main/java/com/lol/ml/starthackapi/StartHackApiApplication.java
+++ src/main/java/com/lol/ml/starthackapi/StartHackApiApplication.java
@@ -25,20 +25,24 @@ public class StartHackApiApplication {
     }
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(StartHackApiApplication.class);
-        Map<String, Object> properties = new HashMap<>();
-
-        String port = System.getenv("PORT"); // Get Heroku's assigned port
-        if (port == null) {
-            port = "8080"; // Default for local testing
-        }
-
-        System.out.println("⚡ Starting application on port: " + port);
-        properties.put("server.port", port);
-        app.setDefaultProperties(properties);
-
-        app.run(args);
+        SpringApplication.run(StartHackApiApplication.class, args);
     }
+
+    //public static void main(String[] args) {
+    //    SpringApplication app = new SpringApplication(StartHackApiApplication.class);
+    //    Map<String, Object> properties = new HashMap<>();
+//
+    //    String port = System.getenv("PORT"); // Get Heroku's assigned port
+    //    if (port == null) {
+    //        port = "8080"; // Default for local testing
+    //    }
+//
+    //    System.out.println("⚡ Star ting application on port: " + port);
+    //    properties.put("server.port", port);
+    //    app.setDefaultProperties(properties);
+//
+    //    app.run(args);
+    //}
 
 
     @GetMapping("/ask_ai")
