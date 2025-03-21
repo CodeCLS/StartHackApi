6f50fa02f4ecdbbe8f5fdedac280f9f725a7ccee
Senan <senanaslan29@t-online.de>
2025-03-21 02:02:09 +0100

    test

--- src/main/java/com/lol/ml/starthackapi/LiveTextWebSocketHandler.java
+++ src/main/java/com/lol/ml/starthackapi/LiveTextWebSocketHandler.java
@@ -1,6 +1,5 @@
 package com.lol.ml.starthackapi;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
@@ -11,46 +10,35 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
-import java.time.Duration;
-import java.time.Instant;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+@EnableScheduling
 @Component
 public class LiveTextWebSocketHandler extends TextWebSocketHandler {
-    private volatile Instant lastMessageTime = Instant.now();
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-
     private static WebSocketSession session = null;
+    PromptApiRepo promptApiRepo = new PromptApiRepo();
 
     private String textMessage = "";
     private String voiceMessage = "";
-
-    public LiveTextWebSocketHandler() {
-        scheduler.scheduleAtFixedRate(this::checkInactivity, 5, 5, TimeUnit.SECONDS);
-    }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         LiveTextWebSocketHandler.session = session;
         System.out.println("Connection to session " + session.getId() + " established.");
-        session.sendMessage(new TextMessage("Hello Client!"));
+        //session.sendMessage(new TextMessage("Hello Client!"));
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
-        lastMessageTime = Instant.now();
 
         //String json = "{\"channel\": \"textchannel\", \"content\": \"textcontent\"}";
 
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = objectMapper.readTree(payload);
-        System.out.println("52"  + jsonNode + " " + lastMessageTime + " " +textMessage);
 
         String channel = jsonNode.get("channel").asText();
         String content = jsonNode.get("content").asText();
@@ -60,7 +48,7 @@ public class LiveTextWebSocketHandler extends TextWebSocketHandler {
         } else {
             voiceMessage = voiceMessage + " " + content;
         }
-        System.out.println("62"  + jsonNode + " " + lastMessageTime + " " +textMessage + " " + voiceMessage + " " + channel + " " + content);
+        System.out.println("62"  + jsonNode + " " +textMessage + " " + voiceMessage + " " + channel + " " + content);
 
 
 
@@ -78,46 +66,25 @@ public class LiveTextWebSocketHandler extends TextWebSocketHandler {
         }
     }
 
-    @Scheduled(fixedRate = 5000)
-    private void checkInactivity() {
-        System.out.println("81"  + " " + lastMessageTime + " " +textMessage);
-
-        Instant now = Instant.now();
-        long secondsSinceLastMessage = Duration.between(lastMessageTime, now).getSeconds();
-
-        if (secondsSinceLastMessage > 10) { // Falls 10 Sekunden keine Nachricht kam
-            System.out.println("⚠️ Warning: No message received for " + secondsSinceLastMessage + " seconds!");
-
-            returnPrediction(voiceMessage);
-
-            voiceMessage = "";
-        }
-    }
-
-    private void returnPrediction (String message){
-        PromptApiRepo promptApiRepo = new PromptApiRepo();
-
+    @Scheduled(fixedRate = 15000, initialDelay = 15000)
+    private void returnPrediction (){
         String toGemini = "We would like you to make a Prediction of the next Question our customer could" +
                 "ask us. If possible it should be in the financial context. Please keep it as shortly and precisely" +
-                "as possible. Here is a snipped of our last conversation: " + message;
+                "as possible. Here is a snipped of our last conversation: " + voiceMessage;
+
+        voiceMessage = "";
 
         List<String> geminiResponse = promptApiRepo.callGeminiAPI(toGemini);
-        System.out.println("103"  + " " + lastMessageTime + " " +textMessage);
 
         String combinedString = String.join(" ", geminiResponse);
-        System.out.println("106"  + " " + lastMessageTime + " " +textMessage);
 
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonString;
         try {
-            System.out.println("111"  + " " + lastMessageTime + " " +textMessage);
-
-            jsonString = objectMapper.writeValueAsString(Map.of("content", combinedString));
+            jsonString = objectMapper.writeValueAsString(Map.of("content", combinedString, "tag", "prediction"));
             System.out.println(jsonString);
             sendMessageToClient(jsonString);
         } catch (IOException e) {
-            System.out.println("117"  + " " + lastMessageTime + " " +textMessage);
-
             throw new RuntimeException(e);
         }
     }
